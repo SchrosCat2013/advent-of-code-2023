9713cf114d6bfb2bc3e472fb32cd10bde6db37e0
Kieran Molloy <kieran.molloy@pushpay.com>
2023-12-06 18:15:48 +1300

    Day 5 part 1

--- shared.fs
+++ shared.fs
@@ -1,6 +1,24 @@
+open System.Text.RegularExpressions
+
 module Shared =
     [<Struct>]
     type Point = {
         x: int
         y: int
     }
+
+    let splitBy<'T when 'T : equality> (separator: 'T) (source: 'T seq) =
+        use enumerator = source.GetEnumerator()
+        seq {
+            let mutable cont = enumerator.MoveNext()
+            while cont do
+                yield seq {
+                    while cont && not (enumerator.Current = separator) do
+                        yield enumerator.Current
+                        cont <- enumerator.MoveNext()
+                } |> Array.ofSeq
+
+                if cont then
+                    cont <- enumerator.MoveNext()
+                    if not cont then yield [||]
+        }
